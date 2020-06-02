395487af27b5af9f39012543024ef248bb7c2664
hanjx <hanjx@fenbi.com>
2020-06-02 17:29:30 +0800

    add count down view demo

--- app/src/main/java/com/hanjx/demo/MainActivity.java
+++ app/src/main/java/com/hanjx/demo/MainActivity.java
@@ -4,11 +4,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
+import com.hanjx.ui.CountDownView;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        CountDownView countDownView = findViewById(R.id.count_down_view);
+        countDownView.setOnClickListener(v -> countDownView.start());
     }
 }
